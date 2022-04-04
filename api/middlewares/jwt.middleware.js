fa3ab4b6918e16e71e6c4152865d96660f98cbf1
HaoHuynh <hao152903@gmail.com>
2022-04-04 16:24:42 +0700

    refactor: verify depend on time.

--- api/middlewares/jwt.middleware.js
+++ api/middlewares/jwt.middleware.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { resMsg } = require("../utils/res.utils");
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SERCET;
+const TOKEN_LIFE = process.env.ACCESS_TOKEN_LIFE;
 
 const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
   if (token) {
     try {
-      const decoded = jwt.verify(token.slice(7, token.length), ACCESS_TOKEN);
+      const decoded = jwt.verify(token.slice(7, token.length), ACCESS_TOKEN, {
+        "tokenLife": TOKEN_LIFE
+      });
       req.username = decoded.username;
     } catch (error) {
       return res.status(401).send(resMsg(4007, {}));
